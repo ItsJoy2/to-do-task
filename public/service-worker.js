3bb4fdc68600cea0f2dfeebb45a92f95f06081c1
AJ <edulife1999@gmail.com>
2026-04-29 17:08:17 +0600

    changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,15 @@
 const CACHE_NAME = "edulife-todo-app-v2";
-const urlsToCache = [
-    "/",
-    "/login",
-    "css/app.css",
-    "js/app.js"
-];
+const urlsToCache = ["/"];
 
 self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(clients.claim());
 });
 
 self.addEventListener("fetch", event => {
